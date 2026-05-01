0fc2c3aa73ef433fe116d373bf7cf40ca16116aa
Kminseo1203 <kmimseo1203@gmail.com>
2026-05-01 21:34:50 +0900

    Update google.js

--- functions/api/auth/google.js
+++ functions/api/auth/google.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   const { env } = context;
-  const redirectUri = `${env.BASE_URL}/functions/api/auth/callback`;
+  const redirectUri = `${env.BASE_URL}/functions/api/auth/google`;
   const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
   url.searchParams.set('client_id', env.GOOGLE_CLIENT_ID);
   url.searchParams.set('redirect_uri', redirectUri);
