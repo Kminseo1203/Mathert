65abdce08595bcc80118d2648ce4c810317b6bc7
Kminseo1203 <kmimseo1203@gmail.com>
2026-05-01 21:37:29 +0900

    Update google.js

--- functions/api/auth/google.js
+++ functions/api/auth/google.js
@@ -1,10 +1,15 @@
 export async function onRequest(context) {
-  const { env } = context;
-  const redirectUri = `${env.BASE_URL}/functions/api/auth/google`;
+  const clientId = "849893014181-p492lse5lvr230l9ecd6jc99go98nght.apps.googleusercontent.com";
+  const redirectUri = "https://mathert.pages.dev/functions/api/auth/google";
+
   const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-  url.searchParams.set('client_id', env.GOOGLE_CLIENT_ID);
+  
+  url.searchParams.set('client_id', clientId);
   url.searchParams.set('redirect_uri', redirectUri);
   url.searchParams.set('response_type', 'code');
   url.searchParams.set('scope', 'openid email profile');
+  url.searchParams.set('prompt', 'select_account');
+
+  // 즉시 구글로 리다이렉트
   return Response.redirect(url.toString(), 302);
 }
